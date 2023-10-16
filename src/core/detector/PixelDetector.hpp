4359f280819a5cd2a9352cb264bf0b503ad2b5ae
Mihail Bogdan Blidaru <m.blidaru@cern.ch>
2023-10-16 12:21:39 +0200

    Solving the overwriting of virtual function

--- src/core/detector/PixelDetector.hpp
+++ src/core/detector/PixelDetector.hpp
@@ -96,7 +96,7 @@ namespace corryvreckan {
         double getColumn(PositionVector3D<Cartesian3D<double>> localPosition) const override;
 
         // Function to get local position from column (x) and row (y) coordinates
-        virtual PositionVector3D<Cartesian3D<double>> getLocalPosition(double column, double row) const;
+        PositionVector3D<Cartesian3D<double>> getLocalPosition(double column, double row) const override;
 
         // Function to get row and column of pixel
         std::pair<int, int> getInterceptPixel(PositionVector3D<Cartesian3D<double>> localPosition) const override;
